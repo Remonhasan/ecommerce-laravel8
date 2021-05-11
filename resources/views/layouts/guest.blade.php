943956b37ece09b0fdc2d31c7fe678b6f9490cf5
remonhasan <remonhasan041@gmail.com>
2021-05-11 21:56:11 +0600

    updated

--- resources/views/layouts/guest.blade.php
+++ resources/views/layouts/guest.blade.php
@@ -1,4 +1,30 @@
-<!DOCTYPE html>
+
+{{--<!DOCTYPE html>
+<html lang="{{ str_replace('_', '-', app()->getLocale()) }}">
+    <head>
+        <meta charset="utf-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1">
+        <meta name="csrf-token" content="{{ csrf_token() }}">
+
+        <title>{{ config('app.name', 'Laravel') }}</title>
+
+        <!-- Fonts -->
+        <link rel="stylesheet" href="https://fonts.googleapis.com/css2?family=Nunito:wght@400;600;700&display=swap">
+
+        <!-- Styles -->
+        <link rel="stylesheet" href="{{ mix('css/app.css') }}">
+
+        <!-- Scripts -->
+        <script src="{{ mix('js/app.js') }}" defer></script>
+    </head>
+    <body>
+        <div class="font-sans text-gray-900 antialiased">
+            {{ $slot }}
+        </div>
+    </body>
+</html>
+--}}
+    <!DOCTYPE html>
 <html lang="en">
 <head>
     <meta charset="utf-8">
@@ -12,11 +38,10 @@
     <link rel="stylesheet" type="text/css" href="{{asset('assets/css/font-awesome.min.css')}}">
     <link rel="stylesheet" type="text/css" href="{{asset('assets/css/bootstrap.min.css')}}">
     <link rel="stylesheet" type="text/css" href="{{asset('assets/css/owl.carousel.min.css')}}">
-    <link rel="stylesheet" type="text/css" href="{{asset('assets/css/flexslider.css')}}">
     <link rel="stylesheet" type="text/css" href="{{asset('assets/css/chosen.min.css')}}">
     <link rel="stylesheet" type="text/css" href="{{asset('assets/css/style.css')}}">
     <link rel="stylesheet" type="text/css" href="{{asset('assets/css/color-01.css')}}">
-   @livewireStyles
+    @livewireStyles
 </head>
 <body class="home-page home-01 ">
 
@@ -100,11 +125,11 @@
                                             </ul>
                                         </li>
                                     @endif
-                                    @else
+                                @else
                                     <li class="menu-item" ><a title="Register or Login" href="{{route('login')}}">Login</a></li>
                                     <li class="menu-item" ><a title="Register or Login" href="{{route('register')}}">Register</a></li>
-                                    @endif
                                 @endif
+                            @endif
                         </ul>
                     </div>
                 </div>
@@ -490,3 +515,4 @@
 @livewireScripts
 </body>
 </html>
+
